97ed1c398107ad324696b0289894ef6d2c93feae
alexezm1 <alex_speed_96@hotmail.com>
2023-10-24 11:13:04 -0700

    user logout

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { getAuth } from "firebase/auth";
 
 function Profile() {
-  return <div>Profile</div>;
+  const auth = getAuth();
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  });
+
+  const { name, email } = formData;
+
+  const logOut = () => {
+    auth.signOut();
+    navigate("/sign-in");
+  };
+
+  return (
+    <div className="profile">
+      <header className="profileHeader">
+        <p className="pageHeader">My Profile</p>
+        <button type="button" className="logOut" onClick={logOut}>
+          Log Out
+        </button>
+      </header>
+    </div>
+  );
 }
 
 export default Profile;
