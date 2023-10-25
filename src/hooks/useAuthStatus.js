d1cc129aaa7d99cb92fc39a2d406af6d0ea2d459
alexezm1 <alex_speed_96@hotmail.com>
2023-10-24 17:48:35 -0700

    useAuth hook to check wether user is signed in

---
+++ src/hooks/useAuthStatus.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+export const useAuthStatus = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setLoggedIn(true);
+      }
+      setLoading(false);
+    });
+
+    return unsubscribe;
+  }, []);
+  return { loggedIn, loading };
+};
