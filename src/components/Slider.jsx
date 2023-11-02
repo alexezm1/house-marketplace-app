d98297ed5e2efd56d1dda597fe1298f468289fcc
alexezm1 <alex_speed_96@hotmail.com>
2023-11-02 13:08:35 -0700

    Explore Slider component

---
+++ src/components/Slider.jsx
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { collection, getDocs, query, orderBy, limit } from "firebase/firestore";
+import { db } from "../firebase.config";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/bundle";
+import Spinner from "./Spinner";
+
+function Slider() {
+  const [loading, setLoading] = useState(true);
+  const [listings, setListings] = useState(null);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      const listingsRef = collection(db, "listings");
+      const q = query(listingsRef, orderBy("timestamp", "desc"), limit(5));
+      const querySnap = await getDocs(q);
+
+      let listings = [];
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+
+      setListings(listings);
+      setLoading(false);
+    };
+
+    fetchListings();
+  }, []);
+
+  if (loading) {
+    return <Spinner />;
+  }
+  return (
+    listings && (
+      <>
+        <p className="exploreHeading">Recommended</p>
+
+        <Swiper
+          modules={[Navigation, Pagination, Scrollbar, A11y]}
+          slidesPerView={1}
+          pagination={{ clickable: true }}
+          scrollbar={{ draggable: true }}
+          navigation
+          style={{ height: "350px" }}
+        >
+          {listings.map(({ data, id }) => {
+            return (
+              <SwiperSlide
+                key={id}
+                onClick={() => navigate(`/category/${data.type}/${id}`)}
+              >
+                <div
+                  style={{
+                    background: `url(${data.imgUrls}) center no-repeat`,
+                    backgroundSize: "cover",
+                  }}
+                  className="swiperSlideDiv"
+                >
+                  <p className="swiperSlideText">{data.name}</p>
+                  <p className="swiperSlidePrice">
+                    ${data.discountedPrice ?? data.regularPrice}{" "}
+                    {data.type === "rent" && "/ Month"}
+                  </p>
+                </div>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      </>
+    )
+  );
+}
+
+export default Slider;
--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import rentCategoryImage from "../assets/jpg/rentCategoryImage.jpg";
 import sellCategoryImage from "../assets/jpg/sellCategoryImage.jpg";
+import Slider from "../components/Slider";
 
 function Explore() {
   return (
@@ -9,8 +10,7 @@ function Explore() {
       <header>
         <p className="pageHeader">Explore</p>
         <main>
-          {/* Slider */}
-
+          <Slider />
           <p className="exploreCategoryHeading">Categories</p>
           <div className="exploreCategories">
             <Link to="/category/rent">
